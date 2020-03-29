f0af5dabe2d27a99b5922ebdff955f72c6078152
cwiik <toni.rehnman@gmail.com>
2020-03-29 04:57:58 +0300

    rautalankamallin luonti, login sivun tekemistä

---
+++ app/src/components/Popup.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import styled from 'styled-components';
+
+const FullScreen = styled.div`
+    width: 100vw;
+    height: ${window.innerHeight}px;
+    position: absolute;
+    z-index: 100;
+    background-color: #ffffff;
+`;
+
+const Popup = () => {
+    return <FullScreen />;
+};
+
+export default Popup;
