df208038498ade02a38db781210f3a1a069ea19a
cwiik <56187908+cwiik@users.noreply.github.com>
2020-03-29 05:52:14 +0300

    rautalankamallin luonti ja login sivun aloitusta (#18)

--- app/src/App.js
+++ app/src/App.js
@@ -1,45 +1,26 @@
 import React, { useState } from 'react';
-import Lottie from 'lottie-react-web';
-import animation from './animData/data.json';
-import animation2 from './animData/1.json';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+// Components import
+import Login from './components/Login/Login';
+import Game from './components/Game/Game';
 
 const App = () => {
     const [btn, setBtn] = useState(false);
-    const teksti = 'testiä vaan';
-    animation.layers[0].t.d.k[0].s.t = teksti;
-    console.log(animation.layers[0].t.d.k[0].s.t);
 
     return (
         <>
-            <div onClick={() => btn && setBtn(false)}>
-                <Lottie
-                    style={{ height: window.innerHeight, position: 'absolute', width: '40%' }}
-                    options={{
-                        animationData: btn ? animation : animation2,
-                    }}
-                />
-            </div>
-            <div onClick={() => (btn ? setBtn(false) : setBtn(true))}>
-                <Lottie
-                    style={{
-                        height: window.innerHeight,
-                        position: 'absolute',
-                        width: '40%',
-                        left: '60%',
-                    }}
-                    options={{
-                        animationData: btn ? animation : animation2,
-                    }}
-                />
-            </div>
-            <div onClick={() => (btn ? setBtn(false) : setBtn(true))}>
-                <Lottie
-                    style={{ height: window.innerHeight }}
-                    options={{
-                        animationData: animation,
-                    }}
-                />
-            </div>
+            <Router>
+                <Switch>
+                    <Route path='/game'>
+                        <Game />
+                    </Route>
+                    <Route path='/login'>
+                        <Login />
+                    </Route>
+                    <Route path='/'></Route>
+                </Switch>
+            </Router>
         </>
     );
 };
---
+++ app/src/components/Game/Game.js
@@ -0,0 +1,78 @@
+import React from 'react';
+
+// NPM libraries
+import Lottie from 'lottie-react-web';
+import styled from 'styled-components';
+
+// Animation data
+import animation from '../../animData/data.json';
+import animation2 from '../../animData/1.json';
+import trophy from '../../animData/trophy.json';
+
+const Leaderboard = styled.div`
+    background: url(${require('./../../SVG/leaderboard.svg')});
+    width: 100px;
+    height: 100px;
+    position: absolute;
+    left: 5vmin;
+    top: 5vmin;
+    z-index: 2;
+`;
+
+const Game = props => {
+    const teksti = 'testi';
+    animation.layers[0].t.d.k[0].s.t = teksti;
+    console.log(animation.layers[0].t.d.k[0].s.t);
+
+    return (
+        <>
+            <Leaderboard />
+            <div onClick={() => (props.btn ? props.setBtn(false) : props.setBtn(true))}>
+                <Lottie
+                    style={{
+                        width: '100px',
+                        height: '100px',
+                        right: '5vmin',
+                        top: '5vmin',
+                        position: 'absolute',
+                    }}
+                    options={{
+                        animationData: trophy,
+                    }}
+                />
+            </div>
+
+            <div onClick={() => props.btn && props.setBtn(false)}>
+                <Lottie
+                    style={{ height: window.innerHeight, position: 'absolute', width: '40%' }}
+                    options={{
+                        animationData: props.btn ? animation : animation2,
+                    }}
+                />
+            </div>
+            <div onClick={() => (props.btn ? props.setBtn(false) : props.setBtn(true))}>
+                <Lottie
+                    style={{
+                        height: window.innerHeight,
+                        position: 'absolute',
+                        width: '40%',
+                        left: '60%',
+                    }}
+                    options={{
+                        animationData: props.btn ? animation : animation2,
+                    }}
+                />
+            </div>
+            <div onClick={() => (props.btn ? props.setBtn(false) : props.setBtn(true))}>
+                <Lottie
+                    style={{ height: window.innerHeight }}
+                    options={{
+                        animationData: animation,
+                    }}
+                />
+            </div>
+        </>
+    );
+};
+
+export default Game;
