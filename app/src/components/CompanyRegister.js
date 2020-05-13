3efc2a7c042e061ed67a020b51b499ffe129f51b
cwiik <56187908+cwiik@users.noreply.github.com>
2020-05-14 00:32:48 +0300

    env korjauksia

--- app/src/components/CompanyRegister.js
+++ app/src/components/CompanyRegister.js
@@ -71,6 +71,9 @@ const CompanyRegister = (props) => {
                                                 auth: true,
                                                 admin: true,
                                                 JWTtoken: res.data.token,
+
+                                                company: res.data.company,
+
                                             });
                                         }, 5000);
                                     }
