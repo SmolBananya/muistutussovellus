2159d22561b2e73acb3e4d0e229e5011a2245452
cwiik <56187908+cwiik@users.noreply.github.com>
2020-05-20 20:57:39 +0300

    scrum backlogin päivitystä 3 sprintin alkuun (#75)

--- app/src/components/Login.js
+++ app/src/components/Login.js
@@ -26,54 +26,62 @@ const Login = (props) => {
                 alignItems='center'
             >
                 <Grid container direction='row' justify='center' alignItems='center' spacing={1}>
-                    <Grid item xs={10}>
+                    <Grid item xs={12} lg={3}>
                         <Logo src={require('../Images/PointFightLogo.png')} />
                     </Grid>
 
-                    <Grid item xs={12}>
-                        <Textbox
-                            type='text'
-                            placeholder='Käyttäjätunnus'
-                            onChange={(e) => setData({ ...data, email: e.target.value })}
-                        />
-                    </Grid>
-                    <Grid item xs={12}>
-                        <Textbox
-                            type='password'
-                            placeholder='Salasana'
-                            onChange={(e) => setData({ ...data, password: e.target.value })}
-                        />
+                    <Grid container justify='center' alignItems='center' item xs={12}>
+                        <Grid item xs={12} lg={3}>
+                            <Textbox
+                                type='text'
+                                placeholder='Käyttäjätunnus'
+                                onChange={(e) => setData({ ...data, email: e.target.value })}
+                            />
+                        </Grid>
                     </Grid>
 
-                    <Grid item xs={6}>
-                        <Link to='/userregister'>
-                            <Button color={2}>Rekisteröidy</Button>
-                        </Link>
+                    <Grid container justify='center' alignItems='center' item xs={12}>
+                        <Grid item xs={12} lg={3}>
+                            <Textbox
+                                type='password'
+                                placeholder='Salasana'
+                                onChange={(e) => setData({ ...data, password: e.target.value })}
+                            />
+                        </Grid>
                     </Grid>
-                    <Grid item xs={6}>
-                        <Button
-                            color={1}
-                            onClick={async () => {
-                                const res = await API.login(data);
-                                console.log(res.data);
 
-                                if (res.data.auth && res.data.token) {
-                                    setUser({
-                                        ...user,
-                                        auth: res.data.auth,
-                                        JWTtoken: res.data.token,
-                                        admin: res.data.admin,
-                                        company: res.data.company,
-                                    });
-                                    res.data.admin ? history.push('/companymenu') : history.push('/game');
-                                }
-                            }}
-                        >
-                            Kirjaudu
-                        </Button>
+                    <Grid container justify='center' alignItems='center' item xs={12} lg={3} spacing={1}>
+                        <Grid item xs={6} lg={6}>
+                            <Link to='/userregister' style={{ width: '100%' }}>
+                                <Button color={2}>Rekisteröidy</Button>
+                            </Link>
+                        </Grid>
+                        <Grid item xs={6} lg={6}>
+                            <Button
+                                color={1}
+                                onClick={async () => {
+                                    const res = await API.login(data);
+                                    console.log(res.data);
+
+                                    if (res.data.auth && res.data.token) {
+                                        setUser({
+                                            ...user,
+                                            auth: res.data.auth,
+                                            JWTtoken: res.data.token,
+                                            admin: res.data.admin,
+                                            company: res.data.company,
+                                        });
+                                        res.data.admin ? history.push('/companymenu') : history.push('/game');
+                                    }
+                                }}
+                            >
+                                Kirjaudu
+                            </Button>
+                        </Grid>
                     </Grid>
-                    <Grid item xs={12}>
-                        <Link to='/companyregister'>
+
+                    <Grid container justify='center' alignItems='center' xs={12} lg={12}>
+                        <Link to='/companyregister' style={{ width: '100%' }}>
                             <Button color={3}>Rekisteröidy yrityksenä</Button>
                         </Link>
                     </Grid>
